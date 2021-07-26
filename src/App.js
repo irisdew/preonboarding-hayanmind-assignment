35a69ca7290f47ce5ef5a91b0624dfc549153614
YUZAMIN <untmd.crmn@gmail.com>
2021-07-27 00:38:57 +0900

    feat : Add css using styled-components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { useInView } from 'react-intersection-observer';
 import axios from 'axios';
+import styled from 'styled-components';
 import url from './url/index';
 axios.defaults.baseURL = url;
+
+const Root = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #fff;
+`;
+
+const CommentContainer = styled.div`
+  margin-top: 33px;
+`;
+
+const CommentBox = styled.div`
+  width: 500px;
+  background: #f8f9fa;
+  border: 0.5px solid #ced4da;
+  box-sizing: border-box;
+  border-radius: 20px;
+  margin-bottom: 14px;
+  font-size: 18px;
+`;
+
+const CommentBody = styled.div`
+  margin: 20px;
+`;
+const CommentElement = styled.div`
+  margin-bottom: 12px;
+`;
+
+const Title = styled.span`
+  margin-right: 12px;
+  font-weight: 700;
+`;
+const CommentTitle = styled.div`
+  font-weight: 700;
+`;
 
 function App() {
   const [commentInfo, setCommentInfo] = useState([]);
   const [page, setPage] = useState(1);
   const [ref, inView] = useInView({
     root: null,
-    rootMargin: '0px',
+    rootMargin: '10px',
     threshold: 0,
   });
 
   useEffect(() => {
     axios.get(`?_page=${page}&_limit=10`).then((res) => {
-      console.log(res.data);
       setCommentInfo((prev) => [...prev, ...res.data]);
     });
   }, [page]);
@@ -28,18 +64,29 @@ function App() {
   }, [setPage, inView]);
 
   return (
-    <div className="App">
-      {commentInfo.map(function (info, i) {
-        return (
-          <div>
-            <div>Comment Id {info.id}</div>
-            <div>Email {info.email}</div>
-            <div>Comment {info.body}</div>
-          </div>
-        );
-      })}
-      {commentInfo.length !== 0 && <div ref={ref}>hi</div>}
-    </div>
+    <Root className="App">
+      <CommentContainer>
+        {commentInfo.map(function (info, i) {
+          return (
+            <CommentBox key={i}>
+              <CommentBody>
+                <CommentElement>
+                  <Title>Comment Id</Title>
+                  {info.id}
+                </CommentElement>
+                <CommentElement>
+                  <Title>Email</Title> {info.email}
+                </CommentElement>
+                <CommentElement>
+                  <CommentTitle>Comment</CommentTitle> {info.body}
+                </CommentElement>
+              </CommentBody>
+            </CommentBox>
+          );
+        })}
+      </CommentContainer>
+      {commentInfo.length !== 0 && <div ref={ref}> </div>}
+    </Root>
   );
 }
 
