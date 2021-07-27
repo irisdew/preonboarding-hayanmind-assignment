a799745483077bcc36484344d5a410e143dd232d
irisdew <ycleardew@gmail.com>
2021-07-27 09:10:07 +0900

    Feat: Get dummy data from API

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Container, Card } from './App.styles';
 
 function App() {
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/comments', { params: { _page: page, _limit: 5 } })
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [page]);
+
   return (
     <div className="App">
       <Container>
-        <Card>
-          <div>
-            <b>Comment Id</b>
-            <span>1</span>
-          </div>
-          <div>
-            <b>Email</b>
-            <span>Eliseo@gardner.biz</span>
-          </div>
-          <div className="comment-box">
-            <b>Comment</b>
-            <span>
-              laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem
-              quasi\nreiciendis et nam sapiente accusantium
-            </span>
-          </div>
-        </Card>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {data.map((d) => {
+          return (
+            <Card>
+              <div>
+                <b>Comment Id</b>
+                <span>{d.id}</span>
+              </div>
+              <div>
+                <b>Email</b>
+                <span>{d.email}</span>
+              </div>
+              <div className="comment-box">
+                <b>Comment</b>
+                <span>{d.body}</span>
+              </div>
+            </Card>
+          );
+        })}
       </Container>
     </div>
   );
